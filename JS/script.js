fee6f5a90568cd11fc8808b55b0bf267348f70dd
Carlos Alberto <carlosoavila1221@gmail.com>
2023-11-03 06:05:48 -0300

    Add JS

---
+++ JS/script.js
@@ -0,0 +1,12 @@
+// Select elements
+const todoForm = document.querySelector("#todo-form")
+const todoInput = document.querySelector("#todo-input")
+const todoList = document.querySelector("#todo-list")
+const editForm = document.querySelector("#edit-form")
+const editInput = document.querySelector("#edit-input")
+const cancelEditBtn = document.querySelector("#cancel-edit-btn")
+
+// Functions
+
+
+// Events
